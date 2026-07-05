39004ae63fa9b9258a39b14d1f0dbead17356022
Egor Sannikov <exnessys@gmail.com>
2026-07-05 23:42:16 +0300

    feat: solve search-insert-position (LeetCode #35)

---
+++ binary-search/search-insert-position/solution.js
@@ -0,0 +1,28 @@
+/**
+ * Search Insert Position
+ * LeetCode #35
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number}
+ */
+export function searchInsert(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
